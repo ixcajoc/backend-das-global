d19445faf4a7bf0f2f858d3a9b47e653f7d6b709
Denis Ixcajoc <denisixcajoc2001@gmail.com>
2026-07-17 22:06:47 -0600

    comment: delete comments

--- db/pool.js
+++ db/pool.js
@@ -10,13 +10,6 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD || 'user',
 });
 
-// const pool = new Pool({
-//   host: process.env.DB_HOST || 'dpg-d9d7r7jbc2fs73en75f0-a.oregon-postgres.render.com',
-//   port: parseInt(process.env.DB_PORT, 10) || 5432,
-//   database: process.env.DB_NAME || 'das_global',
-//   user: process.env.DB_USER || 'das_global_user',
-//   password: process.env.DB_PASSWORD || 'Oi1ewVkYPryGJKjogbkGUT7vbj3hHrqB',
-// });
 
 pool.on('error', (err) => {
   console.error('Error inesperado en el pool de PostgreSQL:', err);
